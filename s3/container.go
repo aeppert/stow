daee355bcee2ef26ebd6dc8684bb5389634e6f51
Aaron Eppert <aaron.eppert@packetsled.com>
2017-04-14 11:11:04 -0400

    Tweaking graymeta to aeppert for internal usage

--- s3/container.go
+++ s3/container.go
@@ -10,7 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	"github.com/graymeta/stow"
+	"github.com/aeppert/stow"
 	"github.com/pkg/errors"
 )
 
--- s3/item.go
+++ s3/item.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/graymeta/stow"
+	"github.com/aeppert/stow"
 	"github.com/pkg/errors"
 )
 
